980b3c62a24f47dd34381d45814793f432d48db2
Smael Nicolas <nico.smael@gmail.com>
2022-02-02 15:03:22 -0300

    basic app layout

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import styled from "styled-components";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Navbar from "./Components/Navbar/Navbar";
+import ContactUs from "./Components/ContactUs/ContactUs";
+import Home from "./Pages/Home/Home";
+import Resources from "./Pages/Resources/Resources";
+import Login from "./Pages/Login/Login";
 
 function App() {
-	return <AppContainerStyled>{"COMPONENTES DE LA APP"}</AppContainerStyled>;
+	return (
+		<AppContainerStyled>
+			<BrowserRouter>
+				<Navbar></Navbar>
+				<ContactUs></ContactUs>
+				<Routes>
+					<Route exact path='/' element={<Home />} />
+					<Route exact path='/home' element={<Home />} />
+					<Route exact path='/login' element={<Login />} />
+					<Route exact path='/resources' element={<Resources />} />
+				</Routes>
+			</BrowserRouter>
+		</AppContainerStyled>
+	);
 }
 
 const AppContainerStyled = styled.div`
