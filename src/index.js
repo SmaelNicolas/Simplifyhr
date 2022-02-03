d7d0f08a707a34e96883583090d6f66b56eb1d7d
Smael Nicolas <nico.smael@gmail.com>
2022-02-03 11:39:54 -0300

    XXXStyled.js files

--- src/index.js
+++ src/index.js
@@ -1,16 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import { createGlobalStyle } from "styled-components";
-
-const GlobalStyle = createGlobalStyle`
-    *{
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-        scroll-behavior: smooth;
-    }
-`;
+import { GlobalStyle } from "./GlobalStyled";
 
 ReactDOM.render(
 	<React.StrictMode>
