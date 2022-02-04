ca60938c86cdc1a41ab1e6bd9fe3699f6ea86773
Smael Nicolas <nico.smael@gmail.com>
2022-02-04 14:36:33 -0300

    title styled

--- src/pages/Home/Services/ServicesStyled.js
+++ src/pages/Home/Services/ServicesStyled.js
@@ -22,3 +22,15 @@ export const TitleContainer = styled.div`
 	align-items: center;
 	padding: 30px;
 `;
+
+export const TitleStyled = styled.div`
+	width: ${({ width }) => width || "100%"};
+	height: ${({ height }) => height || "auto"};
+	padding: ${({ padding }) => padding || "25px 0px"};
+	color: ${({ color }) => color || "black"};
+	background-color: ${({ backgroundColor }) => backgroundColor || "none"};
+	font-size: ${({ fontSize }) => fontSize || "45px"};
+	font-weight: ${({ fontWeight }) => fontWeight || "800"};
+	letter-spacing: ${({ letterSpacing }) => letterSpacing || "0.4px"};
+	text-align: ${({ textAlign }) => textAlign || "center"};
+`;
