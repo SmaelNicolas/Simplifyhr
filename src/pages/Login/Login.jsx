aba960e3e3c89adeade98225a883ea1c062f44ad
Smael Nicolas <nico.smael@gmail.com>
2022-02-03 21:15:50 -0300

    login form container

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,9 +1,11 @@
 import { TitleStyled } from "./LoginStyled";
+import { FormStyled } from "./LoginStyled";
 
 function Login() {
 	return (
 		<>
 			<TitleStyled>Login</TitleStyled>
+			<FormStyled method='post'> </FormStyled>
 		</>
 	);
 }
--- src/pages/Login/LoginStyled.js
+++ src/pages/Login/LoginStyled.js
@@ -11,3 +11,17 @@ export const TitleStyled = styled.div`
 	letter-spacing: ${({ letterSpacing }) => letterSpacing || "0.4px"};
 	text-align: ${({ textAlign }) => textAlign || "center"};
 `;
+
+export const FormStyled = styled.form`
+	width: 320px;
+	height: 400px;
+	background-color: #000000d1;
+	display: flex;
+	flex-direction: column;
+
+	justify-content: space-evenly;
+	align-items: center;
+
+	border: 1px solid black;
+	border-radius: 6px;
+`;
