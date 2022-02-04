e4b519f5ee3277f2cdfbaf39613405cc0075a821
Smael Nicolas <nico.smael@gmail.com>
2022-02-04 14:46:34 -0300

    services section layout & title

--- src/pages/Home/Services/Services.jsx
+++ src/pages/Home/Services/Services.jsx
@@ -1,11 +1,23 @@
-import { ServicesContainer, TitleContainer } from "./ServicesStyled";
+import {
+	ServicesContainer,
+	TitleContainer,
+	TitleStyled,
+} from "./ServicesStyled";
+
+import Cards from "./Cards/Cards";
 
 function Services() {
 	return (
-		<ServicesContainer backgroundColor='blue'>
+		<ServicesContainer>
 			<TitleContainer>
-				<TitleStyled>StartUp up is in our DNA</TitleStyled>
+				<TitleStyled color='#524ef8'>
+					StartUp up is in our DNA
+				</TitleStyled>
+				<TitleStyled color='#524ef8'>
+					All Services on Succes
+				</TitleStyled>
 			</TitleContainer>
+			<Cards></Cards>
 		</ServicesContainer>
 	);
 }
