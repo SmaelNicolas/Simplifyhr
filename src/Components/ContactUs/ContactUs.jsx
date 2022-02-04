4b0e4ae976162fd9584e5f8489182f059bb9b088
Smael Nicolas <nico.smael@gmail.com>
2022-02-04 12:43:44 -0300

    contct us items , close and form

--- src/Components/ContactUs/ContactUs.jsx
+++ src/Components/ContactUs/ContactUs.jsx
@@ -1,7 +1,20 @@
-import { FormContainer } from "./ContactUsStyled";
+import {
+	ContactUsContainer,
+	FormContainer,
+	CloseForm,
+} from "./ContactUsStyled";
+
+import ContactUsFormItems from "./ContactUsFormItems/ContactUsFormItems";
 
 function ContactUs() {
-	return <FormContainer></FormContainer>;
+	return (
+		<ContactUsContainer>
+			<FormContainer>
+				<CloseForm> X </CloseForm>
+				<ContactUsFormItems></ContactUsFormItems>
+			</FormContainer>
+		</ContactUsContainer>
+	);
 }
 
 export default ContactUs;
--- src/Components/ContactUs/ContactUsStyled.js
+++ src/Components/ContactUs/ContactUsStyled.js
@@ -1,10 +1,40 @@
 import styled from "styled-components";
 
-export const FormContainer = styled.div`
+export const ContactUsContainer = styled.div`
 	width: 100%;
 	height: 100vh;
 	background-color: #000000d1;
 	display: flex;
 	justify-content: center;
 	align-items: center;
+`;
+
+export const FormContainer = styled.div`
+	max-width: 800px;
+	height: auto;
+	display: flex;
+	flex-direction: row;
+	flex-wrap: wrap;
+
+	justify-content: flex-start;
+	align-items: flex-start;
+
+	border: 1px solid black;
+	border-radius: 6px;
+	padding: 20px;
+	background-color: black;
+
+	gap: 10px;
+	position: relative;
+`;
+
+export const CloseForm = styled.div`
+	width: 20px;
+	height: 30px;
+	position: absolute;
+	right: 5px;
+	top: 5px;
+	color: white;
+	font-size: 20px;
+	cursor: pointer;
 `;
