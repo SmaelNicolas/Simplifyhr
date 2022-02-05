26dd40cef0c1e52c25e980db67606fe14cb727de
Smael Nicolas <nico.smael@gmail.com>
2022-02-04 21:29:12 -0300

    connect create blog post with firebase , create english or spanish posts

--- src/pages/Login/BlogPostScreen/FormBlogItems/FormBlogItems.jsx
+++ src/pages/Login/BlogPostScreen/FormBlogItems/FormBlogItems.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import { useState } from "react";
+
 import {
 	Labels,
 	LanguageContainer,
@@ -9,7 +10,30 @@ import {
 	Submit,
 } from "./FormBlogItemsStyled";
 
+import database from "../../../../Firebase/Firebase";
+import { doc, setDoc } from "firebase/firestore";
+import { v4 as uuidv4 } from "uuid";
+
 function FormBlogItems() {
+	const [title, setTitle] = useState();
+	const [body, setBody] = useState();
+	const [imgUrl, setImgUrl] = useState();
+	const [language, setLanguage] = useState(true);
+
+	function generateBlogPost() {
+		const post = {
+			date: new Date(),
+			title: title,
+			body: body,
+			imgUrl: imgUrl,
+		};
+
+		// Add a new document in collection "blogPostEnglish" "blogPostSpanish"
+		language
+			? setDoc(doc(database, "blogPostsEnglish", uuidv4()), post)
+			: setDoc(doc(database, "blogPostsSpanish", uuidv4()), post);
+	}
+
 	return (
 		<>
 			<Labels htmlFor='postTitle'>Language </Labels>
@@ -19,6 +43,10 @@ function FormBlogItems() {
 					id='language1'
 					name='language'
 					value='english'
+					defaultChecked={true}
+					onChange={() => {
+						setLanguage(!language);
+					}}
 				/>
 				<RadioLabel htmlFor='contactChoice1'>English</RadioLabel>
 
@@ -27,6 +55,10 @@ function FormBlogItems() {
 					id='language2'
 					name='language'
 					value='español'
+					defaultChecked={false}
+					onChange={() => {
+						setLanguage(!language);
+					}}
 				/>
 				<RadioLabel htmlFor='contactChoice2'>Español</RadioLabel>
 			</LanguageContainer>
@@ -39,6 +71,7 @@ function FormBlogItems() {
 				name='postTitle'
 				placeholder='Post Title'
 				required
+				onChange={(e) => setTitle(e.target.value)}
 			></Inputs>
 
 			<Labels htmlFor='urlImage'>Url Image </Labels>
@@ -49,6 +82,7 @@ function FormBlogItems() {
 				name='urlImage'
 				placeholder='Image Url'
 				required
+				onChange={(e) => setImgUrl(e.target.value)}
 			></Inputs>
 
 			<Labels htmlFor='postContent'>Body </Labels>
@@ -59,11 +93,13 @@ function FormBlogItems() {
 				name='postContent'
 				placeholder='Write something..'
 				required
+				onChange={(e) => setBody(e.target.value)}
 			></InputPostContent>
+
 			<Submit
 				onClick={(e) => {
 					e.preventDefault();
-					console.log("Post Created");
+					generateBlogPost();
 				}}
 				type='submit'
 			>
