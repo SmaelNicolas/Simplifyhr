fb3622d0879fd28e8f81934f0f95a78f0b34ad69
Smael Nicolas <nico.smael@gmail.com>
2022-02-08 20:36:45 -0300

    new container, full vw vh

--- src/Components/ContactUs/ContactUsStyled.js
+++ src/Components/ContactUs/ContactUsStyled.js
@@ -1,13 +1,39 @@
+import { keyframes } from "styled-components";
 import styled from "styled-components";
 
+const opac = keyframes`
+	0%{opacity:0.5}
+	25%{opacity:0.7}
+	50%{opacity:0.9}
+	100%{opacity:1}
+
+
+`;
+
 export const ContactUsContainer = styled.div`
-	width: 100%;
-	min-height: 100vh;
-	background-color: hsl(0, 1%, 77%);
+	width: 100vw;
+	height: 100vh;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	position: fixed;
+	left: 0px;
+	top: 0px;
+	transition: width 2s;
+`;
+
+export const ContactUsContainer2 = styled.a`
+	background-color: rgba(0, 0, 0, 0.555);
+	width: 100vw;
+	height: 100vh;
 	display: flex;
 	justify-content: center;
 	align-items: center;
 	position: absolute;
+	left: 0px;
+	top: 0px;
+	border: none;
+	animation: ${opac} 1s linear;
 `;
 
 export const FormContainer = styled.form`
@@ -30,6 +56,9 @@ export const FormContainer = styled.form`
 
 	gap: 10px;
 	position: relative;
+	z-index: 10;
+	opacity: ${({ opacity }) => opacity || 0};
+	animation: ${opac} 1s linear;
 `;
 
 export const CloseForm = styled.div`
