4bf3c7b2a4f9c84235b290982b36cb0a52d27db8
Smael Nicolas <nico.smael@gmail.com>
2022-02-10 14:18:28 -0300

    new container for why us cards

---
+++ src/pages/Home/WhyUs/CardsContainer/CardsContainer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Cards from "./Cards/Cards";
+import { ContentContainer } from "./CardsContainerStyled";
+
+function CardsContainer() {
+	return (
+		<ContentContainer>
+			<Cards />
+		</ContentContainer>
+	);
+}
+
+export default CardsContainer;
---
+++ src/pages/Home/WhyUs/CardsContainer/CardsContainerStyled.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const ContentContainer = styled.div`
+	width: 1200px;
+	height: 100%;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	gap: 15px;
+`;
