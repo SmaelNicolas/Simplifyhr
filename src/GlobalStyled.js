59cf3d50f1016e2bb8d2b2a279e4ddcdc1fa243b
Smael Nicolas <nico.smael@gmail.com>
2022-02-14 18:00:55 -0300

    root primary color variable & font family Roboto

--- src/GlobalStyled.js
+++ src/GlobalStyled.js
@@ -1,10 +1,18 @@
 import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
+
+     :root {
+        --primaryColor : #524EF8;
+     }
+
+
     *{
         margin: 0;
         padding: 0;
         box-sizing: border-box;
         scroll-behavior: smooth;
+        font-family: 'Roboto', sans-serif;
+
     }
 `;
