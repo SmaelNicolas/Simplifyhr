28e9f71b76010aab2b4c44fb1f40b47583b53493
Smael Nicolas <nico.smael@gmail.com>
2022-02-15 19:00:46 -0300

    colors & media queries

--- src/pages/Login/BlogPostScreen/FormBlogItems/FormBlogItemsStyled.js
+++ src/pages/Login/BlogPostScreen/FormBlogItems/FormBlogItemsStyled.js
@@ -1,20 +1,30 @@
 import styled from "styled-components";
 
-export const LanguageContainer = styled.div`
-	width: 100%;
+export const InputContainer = styled.div`
+	width: 350px;
+	height: 500px;
 	display: flex;
-	flex-direction: row;
-	text-align: center;
-	justify-content: center;
-	align-items: center;
-	gap: 20px;
+	flex-direction: column;
+	justify-content: space-between;
+	align-items: flex-start;
+	@media (max-width: 1089px) {
+		width: 98%;
+	}
 `;
 
-export const RadioLabel = styled.label`
-	font-size: 30px;
-	color: white;
+export const BodyContainer = styled.div`
+	width: 800px;
+	height: 600px;
+
+	display: flex;
+	flex-direction: column;
+	justify-content: space-between;
+	align-items: flex-start;
+	@media (max-width: 1089px) {
+		padding-top: 20px;
+		width: 98%;
+	}
 `;
-export const RadioInputs = styled.input``;
 
 export const Inputs = styled.input`
 	width: 300px;
@@ -22,22 +32,40 @@ export const Inputs = styled.input`
 	border-radius: 3px;
 	padding-left: 20px;
 	width: 80%;
+	@media (max-width: 1089px) {
+		width: 100%;
+	}
 `;
 
 export const InputPostContent = styled.textarea`
-	width: 80%;
-	height: 400px;
+	width: 100%;
+	height: 470px;
 	padding: 15px;
+	resize: none;
 `;
 
 export const Labels = styled.label`
-	color: #ffff;
+	width: 60%;
+
 	font-size: 25px;
-	width: 100%;
-	padding-left: 70px;
+	align-self: flex-start;
+
+	color: var(--primaryColor);
+
+	@media (max-width: 1089px) {
+		width: 100%;
+	}
 `;
 
 export const Submit = styled.button`
 	width: 200px;
 	height: 50px;
+
+	font-size: 20px;
+
+	align-self: flex-end;
+	background-color: var(--primaryColor);
+	color: var(--secondTitleColor);
+
+	border-radius: 28px;
 `;
