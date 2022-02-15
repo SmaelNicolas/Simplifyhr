2c4d583b1401120d92b42851b02855cdb832d853
Smael Nicolas <nico.smael@gmail.com>
2022-02-15 14:54:53 -0300

    remove loop , hard coded cards

--- src/pages/Home/WhyUs/CardsContainer/Cards/Cards.jsx
+++ src/pages/Home/WhyUs/CardsContainer/Cards/Cards.jsx
@@ -1,5 +1,9 @@
-import { useContext, useState } from "react";
+import { useContext } from "react";
 import { LanguageContext } from "../../../../../Context/LanguageContext";
+
+import svg1 from "../../../../../Assest/trust.svg";
+import svg2 from "../../../../../Assest/low.svg";
+import svg3 from "../../../../../Assest/flexible.svg";
 
 import {
 	CardStyled,
@@ -11,31 +15,31 @@ import {
 
 function Cards() {
 	const { data } = useContext(LanguageContext);
-	const ArrayOfWhyUs = Object.entries(data.whyUs.cards);
-
-	const [returnData] = useState([loop()]);
-
-	function loop() {
-		let arr = [];
-		for (let i = 0; i < ArrayOfWhyUs.length; i++) {
-			arr.push(
-				<CardStyled key={`cardWhy${i}`}>
-					<ImgContainer url='https://picsum.photos/200/200' />
-					<TextContainer>
-						<Title>{ArrayOfWhyUs[i][1].title}</Title>
-						<TextContent>{ArrayOfWhyUs[i][1].body}</TextContent>
-					</TextContainer>
-				</CardStyled>
-			);
-		}
-		return arr;
-	}
 
 	return (
 		<>
 			{console.log("RENDER WHY US CARDS")}
-
-			{returnData}
+			<CardStyled alignSelf='flex-end'>
+				<ImgContainer url={svg1} />
+				<TextContainer>
+					<Title>{data.whyUs.cards.card1.title}</Title>
+					<TextContent>{data.whyUs.cards.card1.body}</TextContent>
+				</TextContainer>
+			</CardStyled>
+			<CardStyled alignSelf='flex-start'>
+				<ImgContainer url={svg2} />
+				<TextContainer>
+					<Title>{data.whyUs.cards.card2.title}</Title>
+					<TextContent>{data.whyUs.cards.card2.body}</TextContent>
+				</TextContainer>
+			</CardStyled>
+			<CardStyled alignSelf='center'>
+				<ImgContainer url={svg3} />
+				<TextContainer width='500px'>
+					<Title>{data.whyUs.cards.card3.title}</Title>
+					<TextContent>{data.whyUs.cards.card3.body}</TextContent>
+				</TextContainer>
+			</CardStyled>
 		</>
 	);
 }
