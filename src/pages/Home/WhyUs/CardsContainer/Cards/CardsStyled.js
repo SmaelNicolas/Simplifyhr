cb9a7d3a0e44c6ab658dc27b4f1d121fc81e1d83
Smael Nicolas <nico.smael@gmail.com>
2022-02-15 14:55:29 -0300

    remove nth childs , widths as props

--- src/pages/Home/WhyUs/CardsContainer/Cards/CardsStyled.js
+++ src/pages/Home/WhyUs/CardsContainer/Cards/CardsStyled.js
@@ -1,37 +1,24 @@
 import styled from "styled-components";
 
 export const CardStyled = styled.section`
-	width: 900px;
+	width: auto;
 	display: flex;
 	flex-direction: row;
 	align-self: ${({ alignSelf }) => alignSelf || "none"};
 	gap: 15px;
-
-	:nth-child(1) {
-		align-self: flex-end;
-	}
-	:nth-child(2) {
-		align-self: flex-start;
-	}
-	:nth-child(3) {
-		align-self: center;
-
-		margin-left: 100px;
-	}
 `;
 
 export const ImgContainer = styled.div`
-	width: 100px;
-	height: 100px;
-	background-image: url(${({ url }) => url});
+	width: 200px;
+	height: 200px;
+	background-image: url(${({ url }) => `${url}`});
 	background-size: contain;
 	background-repeat: no-repeat;
 	background-position: center;
 `;
 
 export const TextContainer = styled.div`
-	background-color: green;
-	width: 900px;
+	width: ${({ width }) => width};
 	display: flex;
 	flex-direction: column;
 	justify-content: center;
