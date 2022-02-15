2be779228b933b820962e44390fe313cd29ea769
Smael Nicolas <nico.smael@gmail.com>
2022-02-15 16:11:10 -0300

    media querys & width

--- src/pages/Home/WhyUs/CardsContainer/CardsContainerStyled.js
+++ src/pages/Home/WhyUs/CardsContainer/CardsContainerStyled.js
@@ -1,11 +1,18 @@
 import styled from "styled-components";
 
 export const ContentContainer = styled.div`
-	width: 80%;
-	height: 100%;
+	width: 90%;
 	display: flex;
 	flex-direction: column;
 	justify-content: center;
 	align-items: center;
 	gap: 15px;
+
+	@media (max-width: 1089px) {
+		width: 100%;
+	}
+
+	@media (max-width: 330px) {
+		gap: 50px;
+	}
 `;
