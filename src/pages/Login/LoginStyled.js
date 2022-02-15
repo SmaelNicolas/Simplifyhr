11010943089c65a0ad0450d2121727ee29dc9bb9
Smael Nicolas <nico.smael@gmail.com>
2022-02-15 18:59:56 -0300

    clean code, colors , widtsh

--- src/pages/Login/LoginStyled.js
+++ src/pages/Login/LoginStyled.js
@@ -1,40 +1,34 @@
 import styled from "styled-components";
 
 export const LoginPageStyled = styled.section`
-	width: ${({ width }) => width || "100%"};
-	height: ${({ height }) => height || "auto"};
-	background-color: ${({ backgroundColor }) => backgroundColor || "none"};
-	padding: ${({ padding }) => padding || "100px"};
-	display: ${({ display }) => display || "flex"};
-	flex-direction: ${({ flexDirection }) => flexDirection || "column"};
-	justify-content: ${({ justifyContent }) => justifyContent || "center"};
-	align-items: ${({ alignItems }) => alignItems || "center"};
-	gap: ${({ gap }) => gap || "15px"};
-	border: ${({ border }) => border || "none"};
+	width: 100%;
+	min-height: 80vh;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
 `;
 
 export const TitleStyled = styled.div`
-	width: ${({ width }) => width || "100%"};
-	height: ${({ height }) => height || "auto"};
-	padding: ${({ padding }) => padding || "25px 0px"};
-	color: ${({ color }) => color || "black"};
-	background-color: ${({ backgroundColor }) => backgroundColor || "none"};
-	font-size: ${({ fontSize }) => fontSize || "45px"};
-	font-weight: ${({ fontWeight }) => fontWeight || "800"};
-	letter-spacing: ${({ letterSpacing }) => letterSpacing || "0.4px"};
-	text-align: ${({ textAlign }) => textAlign || "center"};
+	width: 100%;
+	height: auto;
+	padding: 25px 0px;
+	color: var(--primaryColor);
+	font-size: 45px;
+	font-weight: 700;
+	letter-spacing: 0.4px;
+	text-align: center;
 `;
 
 export const FormStyled = styled.form`
 	width: 320px;
 	height: 400px;
-	background-color: #000000d1;
 	display: flex;
 	flex-direction: column;
 
 	justify-content: space-evenly;
 	align-items: center;
 
-	border: 1px solid black;
+	border: 1px solid var(--secondColor);
 	border-radius: 6px;
 `;
