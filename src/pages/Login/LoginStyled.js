1b75cf22d20a5b0d23dcd7846501468c324c5b5f
Smael Nicolas <nico.smael@gmail.com>
2022-02-16 00:07:52 -0300

    fix heigth

--- src/pages/Login/LoginStyled.js
+++ src/pages/Login/LoginStyled.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const LoginPageStyled = styled.section`
 	width: 100%;
-	min-height: 80vh;
+	min-height: 70vh;
 	display: flex;
 	flex-direction: column;
 	justify-content: center;
