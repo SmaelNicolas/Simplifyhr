89e65ce18586050a8bc7ffb40f18bd88b12c94a7
Smael Nicolas <nico.smael@gmail.com>
2022-02-16 10:45:56 -0300

    fix width

--- src/pages/Home/Services/CardsContainer/CardsContainerStyled.js
+++ src/pages/Home/Services/CardsContainer/CardsContainerStyled.js
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 export const CardsContainerStyled = styled.div`
-	max-width: 1000px;
+	max-width: 1500px;
 	height: auto;
 	display: flex;
 	flex-direction: row;
