28e212740577d58e0c4671676d0c7a455cc97ed1
Smael Nicolas <nico.smael@gmail.com>
2022-02-16 12:28:28 -0300

    add media querie

--- src/pages/Login/LoginImages/LoginImagesStyled.js
+++ src/pages/Login/LoginImages/LoginImagesStyled.js
@@ -1,14 +1,19 @@
 import styled from "styled-components";
 
 export const ImageContainer = styled.div`
-	width: 350px;
-	height: 445px;
+	width: 550px;
+	height: 645px;
 	background-image: url(${({ url }) => `${url}`});
 	position: absolute;
 	background-position: center;
 	background-size: contain;
 	background-repeat: no-repeat;
-	left: 0px;
-	bottom: 45px;
+	left: -20px;
 	overflow-x: hidden;
+
+	@media (max-width: 650px) {
+		width: 350px;
+		height: 445px;
+		bottom: 45px;
+	}
 `;
