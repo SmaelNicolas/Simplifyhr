69f0e570a8125f172e195a747fc4853d6f87ff63
fioseia <fio.seia@hotmail.com>
2022-02-18 00:36:55 -0300

    footer height

--- src/Components/Footer/FooterStyled.js
+++ src/Components/Footer/FooterStyled.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 
 export const FooterContainer = styled.div`
 	width: 100%;
-	height: 30vh;
+	height: 40vh;
 	background-color: #000;
 	display: flex;
 	justify-content: space-between;
