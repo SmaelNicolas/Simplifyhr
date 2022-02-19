af41a17282c022734a06a03a6957201f4a398b93
fioseia <fio.seia@hotmail.com>
2022-02-18 22:35:00 -0300

    Post View elements

--- src/pages/Resources/PostView/PostView.jsx
+++ src/pages/Resources/PostView/PostView.jsx
@@ -1,7 +1,59 @@
-import React from "react";
+import { collection } from "firebase/firestore";
+import React, { useContext, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { LanguageContext } from "../../../Context/LanguageContext";
+import database from "../../../Firebase/Firebase";
+import { getPosts } from "../../../Helpers/getPosts";
+import {
+	Author,
+	Body,
+	Container,
+	Image,
+	Info,
+	MainContainer,
+	ReadTime,
+	ReturnButton,
+	ReturnContainer,
+	Title,
+	VerticalLine,
+	Wrapper,
+} from "./PostViewStyle";
 
 const PostView = () => {
-	return <div></div>;
+	const [post, setPost] = useState({});
+	const { postId } = useParams();
+	const { language, setLoading, loading } = useContext(LanguageContext);
+	const postsCollection = collection(database, "blogPostsEnglish");
+
+	useEffect(() => {
+		getPosts(postsCollection, setLoading, loading).then((resultado) => {
+			const res = resultado.find((el) => el.id === postId);
+			setPost(res);
+		});
+		return () => {};
+	}, []);
+	console.log(post);
+	return (
+		<Container>
+			<ReturnContainer>
+				<ReturnButton to={"/resources"}>
+					{language === "English" ? "Return to the blog" : "Regresar al blog"}
+				</ReturnButton>
+			</ReturnContainer>
+			<MainContainer>
+				<Info>
+					<Title>{post.title}</Title>
+					<Wrapper>
+						<Author>{post.author}</Author>
+						<VerticalLine />
+						<ReadTime>{`${post.readTime} min`}</ReadTime>
+					</Wrapper>
+				</Info>
+				<Image src={post.imgUrl} />
+				<Body>{post.body}</Body>
+			</MainContainer>
+		</Container>
+	);
 };
 
 export default PostView;
---
+++ src/pages/Resources/PostView/PostViewStyle.js
@@ -0,0 +1,39 @@
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+export const Container = styled.div`
+width: 100%;
+min-height: 100vh;
+height: auto;
+`
+
+export const ReturnContainer = styled.div`
+width: 100%;
+height: 10vh;
+box-shadow: 0px 1px 1px 1px rgba(158, 154, 154, 0.77);
+display: flex;
+align-items: center;
+justify-content: flex-start;
+`
+
+export const ReturnButton = styled(Link)`
+
+`
+
+export const MainContainer = styled.div``
+
+export const Info = styled.div``
+
+export const Title = styled.h1``
+
+export const Wrapper = styled.div``
+
+export const Author = styled.h2``
+
+export const VerticalLine = styled.div``
+
+export const ReadTime = styled.p``
+
+export const Image = styled.img``
+
+export const Body = styled.p``
