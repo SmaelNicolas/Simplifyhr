dc398f5e6798a4276514077f50334a7d941c43db
fioseia <fio.seia@hotmail.com>
2022-02-22 20:08:17 -0300

    video fixing

--- src/pages/Home/Header/Header.jsx
+++ src/pages/Home/Header/Header.jsx
@@ -2,7 +2,7 @@ import { useContext } from "react";
 import { LanguageContext } from "../../../Context/LanguageContext";
 import Video from "../../../Assets/video.mp4";
 import {
-  Button,
+	Button,
 	HeaderBg,
 	HeaderContainer,
 	HeaderVideo,
@@ -18,16 +18,23 @@ const Header = () => {
 	return (
 		<HeaderContainer id="home">
 			<HeaderBg>
-				<HeaderVideo autoPlay loop muted src={Video} type="video/mp4" />
+				<HeaderVideo
+					autoPlay
+					loop
+					muted
+					src={`${process.env.PUBLIC_URL}${Video}`}
+					type="video/mp4"
+				/>
 			</HeaderBg>
 			<InfoContainer>
 				<Title>{data.header.title}</Title>
 				<Subtitle>{data.header.subTitle}</Subtitle>
-        
 			</InfoContainer>
 			<ImgContainer>
-      <Button href='https://calendly.com/simplifyhr/intro-call'>{data.header.button}</Button>
-      </ImgContainer>
+				<Button href="https://calendly.com/simplifyhr/intro-call">
+					{data.header.button}
+				</Button>
+			</ImgContainer>
 		</HeaderContainer>
 	);
 };
