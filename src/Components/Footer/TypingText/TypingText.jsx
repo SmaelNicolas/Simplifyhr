5f370753d9723f7cf9307aba232a0272fddc07c1
fioseia <fio.seia@hotmail.com>
2022-03-02 20:51:27 -0300

    changes in gsap

--- src/Components/Footer/TypingText/TypingText.jsx
+++ src/Components/Footer/TypingText/TypingText.jsx
@@ -10,7 +10,7 @@ const Text = styled.span``;
 
 const TypingText = ({ data }) => {
 	const [array, setArray] = useState(data.footer.subtitles);
-  let masterTL = gsap.timeline({repeat:-1});
+	let master = gsap.timeline({ repeat: -1 });
 
 	useEffect(() => {
 		const cursor = document.querySelector("#footerCursor");
@@ -18,11 +18,11 @@ const TypingText = ({ data }) => {
 
 		gsap.to(cursor, { opacity: 0, ease: "power2.inOut", repeat: -1 });
 
-	
 		array.forEach((subt) => {
-      let tl = gsap.timeline({repeat:1, yoyo:true, repeatDelay: 2 })
-      .to(text, { duration: 2, text: subt });
-      masterTL.add(tl)
+			let time = gsap
+				.timeline({ repeat: 1, yoyo: true, repeatDelay: 2 })
+				.to(text, { duration: 2, text: subt });
+			master.add(time);
 		});
 	}, []);
 
